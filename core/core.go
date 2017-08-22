3c1295ed3204e58b06d255c6527eeb3c01ffcf20
Arto Bendiken <arto@dryproject.org>
2017-08-22 11:51:37 +0200

    Defined initial core implementations.

---
+++ core/core.go
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+// Package dry provides the DRYlib core implementation for Go.
+package dry
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+
+// Version contains the current package version, as a string.
+const Version = "0.0.0"
+
+////////////////////////////////////////////////////////////////////////////////
