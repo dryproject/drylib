35e1e251e12ccf46c94f59822d6c50696735b1a4
Arto Bendiken <arto@dryproject.org>
2017-09-04 20:45:57 +0200

    Moved all runtimes to their own language-specific repositories.

--- core/runtime.rb/version.rb
+++
@@ -1,7 +0,0 @@
-# This is free and unencumbered software released into the public domain.
-
-################################################################################
-
-module DRY
-  VERSION = '0.0.0' # TODO
-end # DRY
